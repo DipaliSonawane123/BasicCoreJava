c2d2cb32d6902fad3dc3e80632e668aafb15f3c1
Dipali <dipssonawane123@gmail.com>
2022-06-19 21:04:16 +0530

    Added:hormonic number added

---
+++ src/com/corejava/HarmonicNumber.java
@@ -0,0 +1,21 @@
+package com.corejava;
+import java.util.Scanner;
+
+public class HarmonicNumber {
+    public static void main(String[] args) {
+        int N;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Nth value Of Harmonic Number");
+        N = sc.nextInt();
+        sc.close();
+        if (N > 0) {
+            for (int count = 1; count <= N; count++) {
+                System.out.print(1 + "/" + count + " " + "+" + " ");
+            }
+        } else {
+            System.out.println("Invalid Input ");
+        }
+    }
+    }
+
+
